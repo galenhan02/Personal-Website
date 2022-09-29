615377f08dc925519f987d46f56cb409e4defa15
Galen <galen.han02@gmail.com>
2022-09-28 21:54:18 -0700

    Finalized Fun Facts

--- FunFact.js
+++ FunFact.js
@@ -1,4 +1,4 @@
-let facts = ["FF1", "FF2", "FF3"];
+let facts = ["My favorite song at the moment is Do Re Mi by Seventeen", "I love playing the ukulele", "A game I enjoy playing is called TFT"];
 
 export function init() {
     getFacts();
